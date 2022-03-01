350ae718c2b08bc0e737125a7b85be61630b6eac
nadastra <40881721+nadastra@users.noreply.github.com>
2022-03-01 20:29:44 +1100

    Update time-ms.js

--- hello-kubernetes/node-ms/time-ms.js
+++ hello-kubernetes/node-ms/time-ms.js
@@ -28,4 +28,4 @@ app.get('/ports', (_req, res) => {
     res.status(200).send({DAPR_HTTP_PORT: daprPort, DAPR_GRPC_PORT: daprGRPCPort })
 });
 
-app.listen(port, () => console.log(`Time micro service listening on port ${port}!`));
+app.listen(port, () => console.log(`Time micro service listening on port: ${port}!`));
